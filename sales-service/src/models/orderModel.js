08c88c0ebe0e75e0c053924fa453b66f2d96d7bb
mehdichamam <mehdichamam465@gmail.com>
2026-07-27 21:43:46 +0000

    adding order Model

---
+++ sales-service/src/models/orderModel.js
@@ -0,0 +1,30 @@
+const pool = require("../config/db");
+
+// GET ALL CUSTOMER
+
+const getAllOrders = async () =>{
+    const [orders] = await pool.query("select * from orders");
+    return orders
+}
+// GET SPECIFIC customer 
+const getOrderById = async (id) =>{
+    const [order] = await pool.query("select * from orders where id = ?",[id]);
+    return order
+}
+// CREATE A PRODUCT
+const createOrder = async (customer_id) =>{
+    const [order] = await pool.query("insert into orders (customer_id) values (?)",[customer_id]);
+    return order
+}
+// UPDATE A PRODUCT 
+const updateOrder = async (order_id,customer_id) =>{
+    const [order] = await pool.query("update orders set customer_id = ?  where order_id = ?",[order_id,customer_id]);
+    return order
+}
+// REMOVE A PRODUCT
+const removeOrder = async (order_id) => {
+    const [order] = pool.query("Delete From orders where order_id = ? ",[id]);
+    return order
+}
+
+module.exports = {getAllOrders,getOrderById,createOrder,updateOrder,removeOrder}
