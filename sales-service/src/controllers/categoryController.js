289f4d98a1d26bac7af58b2885da8a161069ca8e
mehdichamam <mehdichamam465@gmail.com>
2026-07-27 00:56:34 +0000

    Setting Up Routes

--- sales-service/src/controllers/categoryController.js
+++ sales-service/src/controllers/categoryController.js
@@ -73,4 +73,4 @@ const removeCategory = async (req,res) =>{
     }
 }
 
-module.exports = {getCategory,getCategoryById,createCategory,updateCategory,removeCategory}
+module.exports = {getCategory, getCategoryById , createCategory , updateCategory , removeCategory}
---
+++ sales-service/src/routes/categoryRoutes.js
@@ -0,0 +1,13 @@
+const express = require("express");
+
+const categoryControllers= require("../controllers/categoryController");
+
+const Router = express.Router();
+
+Router.get("/", categoryControllers.getCategory);
+Router.get("/:id" , categoryControllers.getCategoryById);
+Router.post("/", categoryControllers.createCategory);
+Router.put("/:id", categoryControllers.updateCategory);
+Router.delete("/:id" , categoryControllers.removeCategory);
+
+module.exports = Router
