453b361315ca5338a1b8e2112f8f24e9b2d08c2b
mehdichamam <mehdichamam465@gmail.com>
2026-07-27 00:44:36 +0000

    Just mixed Up with models so i fixed them

--- sales-service/src/controllers/categoryController.js
+++ sales-service/src/controllers/categoryController.js
@@ -1,59 +1,27 @@
-const pool = require("../config/db");
+const categoryModel = require("../models/categoryModel");
 
-// GET ALL CATEGORIES
-const getAllCategories = async (req,res) => {
-
+// GET Category
+const getCategory = async (req,res) =>{
     try {
-    const [categorie] = await pool.query("select * from categories");
-    if(categorie.length === 0){
-        return res.status(404).json({
-            message:"Categorie Not Found"
-        });
-    }
-    return res.status(200).json({
-        message:"Categorie Has been Founded Succesufully",
-        categorie
-    })
-    } catch (error) {
-        return res.status(500).json({
-            message:error.message
-        });
-    }
-  
-};
-// GET SPECIFIC CATEGORIE
-const getCategoriesById = async (req,res) =>{
-    try {
-        const {id} = req.params;
+        const category = await categoryModel.getAllCategories();
 
-        const [categorie] = await pool.query("select * from categories where id = ?" , [id] ) ;
-
-        if(categorie.length === 0){
-            return res.status(404).json({
-                message:"Categorie Not Found"
-            });
-        }
-        return res.status(200).json({
-            message:"Categorie Has been Founded Succesufully",
-            categorie
-        })
-
+        if(!category || category.lenght === 0) return res.status(404).json({message:"Category Not Found"});
+        res.status(200).json(category)
     } catch (error) {
         return res.status(500).json({
             message:error.message
         });
     }
 }
-// create A categorie
-const createCategories = async (req,res) =>{
 
+// GET SPECIFIC CATEGORY WITH SPECIFIC ID
+
+const getCategoryById = async (req , res) =>{
     try {
-        const {name} = req.body;
-        const [category] = await pool.query("insert into categories (name) values (?) ", [name]);
-        res.status(201).json({
-            message:"Categorie Created Succesfully",
-            category
-        });
+        const category = await categoryModel.getCategoriesById(req.params.id);
+
+        if(!category || category.lenght === 0) return res.status(404).json({message:"Category Not Found"});
+        res.status(200).json(category);
     } catch (error) {
         return res.status(500).json({
             message:error.message
@@ -62,62 +30,47 @@ const createCategories = async (req,res) =>{
 
 }
 
-// Update Categorie
+// CREATE CATEGORY 
+const createCategory = async (req,res) =>{
+    try{
+        const { name } = req.body;
+
+        const category = categoryModel.createCategories(name);
+        if(category.affectedRows === 0) return res.status(404).json({message : "Category Not found"});
+        res.status(201).json(category)
+    } catch (error) {
+        return res.status(500).json({
+            message:error.message
+        });
+    }
+}
 
 const updateCategory = async (req,res) =>{
     try {
+        const {name} = req.body;
+        const {id} = req.params;
 
-    const {name} = req.body;
-    const  {id} = req.params;
-
-    if(!id){
-        return res.status(404).json({
-            message:"Id Not found"
-        });
-    }
-
-    const [category] = await pool.query("UPDATE categories set name = ? where id = ?",[name,id]);
-    
-    if (category.affectedRows === 0) {
-        return res.status(404).json({
-            message: "Category not found"
-        });
-    }
-    return res.status(200).json({
-        message:"Categorie Updated Succusfully",
-        category
-    });
+        const category = categoryModel.updateCategory(id,name);
+        if(category.affectedRows === 0) return res.status(404).json({message : "Category Not found"});
+        res.status(201).json(category)
     } catch (error) {
-
         return res.status(500).json({
             message:error.message
         });
     }
-
 }
 
-const deleteCatogory = async (req,res) =>{
+const removeCategory = async (req,res) =>{
     try {
         const {id} = req.params;
-        if(!id){
-            return res.status(404).json({
-                message:"Id Not found"
-            });
-        }
-        const [category] = await pool.query("Delete FROM categories where id = ?",[id]);
-        res.status(200).json({
-            message:"Category deleted Succusfully"
-        });
-        if (category.affectedRows === 0) {
-            return res.status(404).json({
-                message: "Category not found"
-        });
-}
+        const category = categoryModel.deleteCatogory(id);
+        if(category.affectedRows === 0) return res.status(404).json({message : "Category Not found"});
+        res.status(201).json(category)
     } catch (error) {
         return res.status(500).json({
             message:error.message
-        });
+        }); 
     }
 }
 
-module.exports = {getAllCategories,getCategoriesById,createCategories,updateCategory,deleteCatogory}
+module.exports
